2d7937caa2cb3590554df48b4a54f4e0f99d2312
Kyle Farrugia <k.farrugia94@gmail.com>
2023-05-16 18:17:35 +0100

    fixed test functions

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_new_item')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def tearDown(self) -> None:
         self.browser.quit()
         
@@ -37,31 +42,21 @@ class NewVisitorTest(unittest.TestCase):
         
         #he hits enter adn the page updates, now lsiting his todo as an item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
-        
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-        '2: Use peacock feathers to make a fly',
-        [row.text for row in rows]
-        )
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         
         #the input box for new items is still there, he enters another to do
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        
         
         #the page refreshes, both items are in the list now
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+
         #he wonders if these todos will be remembered, he notices he has a unique url
         #and some text explaining this
 
