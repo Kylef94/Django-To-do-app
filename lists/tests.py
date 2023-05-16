dc0d6df480d8ab078fb2796c236ab5544e41d9a7
Kyle Farrugia <k.farrugia94@gmail.com>
2023-05-16 17:07:16 +0100

    build out basic page

---
+++ lists/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
+
+class HomePageTest(TestCase):
+    
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
