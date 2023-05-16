b38565829246b361ef419fc964a0d6bd196424d3
Kyle Farrugia <k.farrugia94@gmail.com>
2023-05-16 18:11:30 +0100

    added new todo functionality

--- functional_tests.py
+++ functional_tests.py
@@ -33,30 +33,42 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
         #he enters a task into the text box
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('1: Buy peacock feathers')
         
         #he hits enter adn the page updates, now lsiting his todo as an item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+        '2: Use peacock feathers to make a fly',
+        [row.text for row in rows]
         )
         
         #the input box for new items is still there, he enters another to do
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         
         #the page refreshes, both items are in the list now
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
         #he wonders if these todos will be remembered, he notices he has a unique url
         #and some text explaining this
 
         #he visits that url, the todos are still there
 
         #he is now satisfied
+        self.fail("Finish the test!")
 
 if __name__ == "__main__":
     unittest.main()
