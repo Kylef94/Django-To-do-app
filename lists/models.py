5965230ba4ef4c07ae4305aa7b197a1d2334c0ba
Kyle Farrugia <k.farrugia94@gmail.com>
2023-05-17 21:25:30 +0100

    add list model and views

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+class List(models.Model):
+    name = models.TextField(default='')
+    
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
+
