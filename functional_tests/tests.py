e01f106723c0060f588c20504488d06136891f50
Kyle Farrugia <k.farrugia94@gmail.com>
2023-05-17 17:32:05 +0100

    added test for multiple user lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,23 +4,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
+MAX_WAIT = 10
+
 class NewVisitorTest(LiveServerTestCase):
+    
+    
     
     def setUp(self) -> None:
         self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
-        
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+                
     def tearDown(self) -> None:
         self.browser.quit()
         
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         
         #john hears about a great new to do app and decides to check it out
         self.browser.get(self.live_server_url)
@@ -43,25 +58,64 @@ class NewVisitorTest(LiveServerTestCase):
         
         #he hits enter adn the page updates, now lsiting his todo as an item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
         
         #the input box for new items is still there, he enters another to do
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         
         
         #the page refreshes, both items are in the list now
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         #he wonders if these todos will be remembered, he notices he has a unique url
         #and some text explaining this
+        
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #John starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        #he visits that url, the todos are still there
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        
+        ## new browser to ensure Johns data doesnt come throuhg cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
 
         #he is now satisfied
         self.fail("Finish the test!")
