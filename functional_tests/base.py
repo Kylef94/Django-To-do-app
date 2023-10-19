de24a475d4fc5ac196ad3f2dd78a079d1ef09136
kylef94 <k.farrugia94@gmail.com>
2023-10-19 15:15:19 +0100

    use form in all views, back to working state

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -46,5 +46,5 @@ class FunctionalTest(StaticLiveServerTestCase):
                 time.sleep(0.5)
     
     def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
+        return self.browser.find_element(By.ID, 'id_text')
     
